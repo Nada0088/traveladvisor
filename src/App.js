9b4bd1091799967b7bfd0bc907f0f260d6288636
Nada <nada@Nadas-MBP.Home>
2022-03-20 22:46:50 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+//importing things start creating the layouts
+import { CssBaseline,Grid } from "@material-ui/core"; // CssBaseline normalises the style, fix some padding marging and background colors
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
+
+const App= () => { 
+    return (
+        // create react fragment
+        <> 
+        <CssBaseline/>
+        <Header/>
+        {/* creating an inline style so we create an object by using double curl braces*/}
+        <Grid container spacing={3} style={{width: '100%'}}> 
+            <Grid item xs={12}> {/* xs: full width on mobile devices, */}
+
+            </Grid>
+        </Grid>
+        </>
+    );
+}
+export default App;
